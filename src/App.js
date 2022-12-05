dadad938218bfa5f2c3d9d60a9c5927cb2ead55a
avc285 <avc285@gmail.com>
2022-12-05 12:23:06 +0000

    insert components into App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Articles from "./components/Articles";
+import Footer from "./components/Footer";
+import Logo from "./components/Logo";
+import Nav from "./components/Nav";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Logo />
+            <Nav />
+            <Articles />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
