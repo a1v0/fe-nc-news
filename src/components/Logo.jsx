1cb0e4814f0971d2b0764c6fa3f1d6b70127cd98
avc285 <avc285@gmail.com>
2022-12-05 14:37:09 +0000

    create Logo component

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Logo() {
     return (
         <div className="Logo">
-            <span>a</span>
-            <span>v</span>
-            <span>c</span>
-            <span className="Logo-news">news</span>
+            <Link>
+                <span>a</span>
+                <span>v</span>
+                <span>c</span>
+                <span className="Logo-news">news</span>
+            </Link>
         </div>
     );
 }
