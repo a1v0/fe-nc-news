d3fdd433da5ae57e1dd9546421b44cd06a829e7b
avc285 <avc285@gmail.com>
2022-12-05 15:36:51 +0000

    display articles based on selected topic

--- src/api.js
+++ src/api.js
@@ -4,9 +4,11 @@ const apiConnection = axios.create({
     baseURL: "https://long-yak-baseball-cap.cyclic.app/api/"
 });
 
-export const getArticles = () => {
+export const getArticles = (topic_id) => {
     return apiConnection
-        .get("/articles?sort_by=article_id&order=desc")
+        .get("/articles?sort_by=article_id&order=desc", {
+            params: { topic: topic_id }
+        })
         .then((response) => {
             return response.data.articles;
         })
