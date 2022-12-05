73338cb18e33458a28b9d9ab61f509a3896abbc9
avc285 <avc285@gmail.com>
2022-12-05 15:46:11 +0000

    add getArticle function

--- src/api.js
+++ src/api.js
@@ -9,11 +9,19 @@ export const getArticles = (topic_id) => {
         .get("/articles?sort_by=article_id&order=desc", {
             params: { topic: topic_id }
         })
-        .then((response) => {
-            return response.data.articles;
+        .then(({ data: { articles } }) => {
+            return articles;
         })
         .catch((err) => {
             console.log("ERROR!", err);
+        });
+};
+
+export const getArticle = (article_id) => {
+    return apiConnection
+        .get(`/articles/${article_id}`)
+        .then(({ data: { article } }) => {
+            console.log(article);
         });
 };
 
