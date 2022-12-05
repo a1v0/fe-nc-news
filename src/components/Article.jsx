35ee2750bc736c0dcedce6ca6bd10b3561064a64
avc285 <avc285@gmail.com>
2022-12-05 16:04:54 +0000

    complete Article component incl styling

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,13 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getArticle } from "../api";
+import PlaceholderImage from "../misc/elementor-placeholder-image.jpg";
 
 export default function Article() {
     const { article_id } = useParams();
+    const [article, setArticle] = useState({});
 
     useEffect(() => {
-        getArticle(article_id);
+        getArticle(article_id).then((article) => {
+            setArticle(article);
+        });
     }, [article_id]);
 
-    return <div className="Article">Article</div>;
+    return (
+        <article className="Article">
+            <h1>{article.title}</h1>
+            <div className="author-and-topic">
+                <p>
+                    <strong>&mdash; {article.author}</strong> (author)
+                </p>
+                <p>#{article.topic}</p>
+            </div>
+            <img src={PlaceholderImage} alt="placeholder" />
+            <p className="article-body">{article.body}</p>
+        </article>
+    );
 }
