f56a6c19b525a288c83a77ef5db768402b793dbb
avc285 <avc285@gmail.com>
2022-12-05 12:22:49 +0000

    create blank components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Footer() {
+    return <div>FOOTER STUFF, e.g. links and copyright</div>;
+}
---
+++ src/components/Nav.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Nav() {
+    return <div className="Nav">LIST OF TOPICS | USER PROFILE STUFF</div>;
+}
