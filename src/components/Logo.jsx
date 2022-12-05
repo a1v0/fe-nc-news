a7d3e6c0a2dd692ba461e8699744f5a0b046cd4d
avc285 <avc285@gmail.com>
2022-12-05 12:22:37 +0000

    style logo

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Logo() {
+    return (
+        <div className="Logo">
+            <span>a</span>
+            <span>v</span>
+            <span>c</span>
+            <span className="Logo-news">news</span>
+        </div>
+    );
+}
