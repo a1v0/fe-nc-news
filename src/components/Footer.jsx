444a979976d07dd4d5d122b60b8540369cd28620
avc285 <avc285@gmail.com>
2022-12-05 15:00:00 +0000

    create draft Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
-    return <div>FOOTER STUFF, e.g. links and copyright</div>;
+    // FOOTER STUFF, e.g. links and copyright
+    return (
+        <div className="Footer">
+            Copyright &copy; 2022 <strong>AVC News</strong>.<br />
+            <Link to="/">Home</Link> | <Link to="/">Topics</Link>
+        </div>
+    );
 }
