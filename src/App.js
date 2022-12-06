7608ff3d440352815ee75663a3f773b5d2b7ebbe
avc285 <avc285@gmail.com>
2022-12-06 14:26:39 +0000

    add route to login page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Article from "./components/Article";
 import Articles from "./components/Articles";
 import Footer from "./components/Footer";
+import Login from "./components/Login";
 import Logo from "./components/Logo";
 import Nav from "./components/Nav";
 import { UserProvider } from "./contexts/UserProvider";
@@ -17,6 +18,7 @@ function App() {
                     <Route path="/" element={<Articles />} />
                     <Route path="/topics/:topic_id" element={<Articles />} />
                     <Route path="/articles/:article_id" element={<Article />} />
+                    <Route path="/user/login" element={<Login />} />
                 </Routes>
             </UserProvider>
             <Footer />
