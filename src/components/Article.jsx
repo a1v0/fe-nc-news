74c36e801b979ea1174b4690570370d09379c644
avc285 <avc285@gmail.com>
2022-12-07 09:06:27 +0000

    render suitable photo for each topic

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getArticle } from "../api";
 import Comments from "./Comments";
+import cookingPhoto from "../misc/maarten-van-den-heuvel-EzH46XCDQRY-unsplash.jpg";
+import codingPhoto from "../misc/markus-spiske-cvBBO4PzWPg-unsplash.jpg";
+import footballPhoto from "../misc/thomas-serer-r-xKieMqL34-unsplash.jpg";
 
 export default function Article() {
     const { article_id } = useParams();
@@ -19,8 +22,26 @@ export default function Article() {
         setIsLoading(false);
     }, [article]);
 
+    let photoSrc, photoAlt;
+    switch (article.topic) {
+        case "cooking":
+            photoSrc = cookingPhoto;
+            photoAlt = "colourful cookery";
+            break;
+        case "coding":
+            photoSrc = codingPhoto;
+            photoAlt = "messy computer code";
+            break;
+        case "football":
+        // if anything goes awry, the default will render a football photo
+        default:
+            photoSrc = footballPhoto;
+            photoAlt = "football stadium";
+            break;
+    }
+
     return (
-        <>
+        <main className="article-parent">
             <article className="Article">
                 {!isLoading ? (
                     <>
@@ -33,10 +54,7 @@ export default function Article() {
                             <p>#{article.topic}</p>
                         </div>
                         <section>
-                            <img
-                                src="https://picsum.photos/500/300"
-                                alt="randomly generated image"
-                            />
+                            <img src={photoSrc} alt={photoAlt} />
                             <p className="article-body">{article.body}</p>
                         </section>
                     </>
@@ -45,6 +63,6 @@ export default function Article() {
                 )}
             </article>
             <Comments article_id={article_id} />
-        </>
+        </main>
     );
 }
--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { getArticles } from "../api";
+import cookingPhoto from "../misc/maarten-van-den-heuvel-EzH46XCDQRY-unsplash.jpg";
+import codingPhoto from "../misc/markus-spiske-cvBBO4PzWPg-unsplash.jpg";
+import footballPhoto from "../misc/thomas-serer-r-xKieMqL34-unsplash.jpg";
 
 export default function Articles() {
     const { topic_id } = useParams();
@@ -22,13 +25,27 @@ export default function Articles() {
         <ul className="Articles">
             {!isLoading ? (
                 articles.map((article) => {
+                    let photoSrc, photoAlt;
+                    switch (article.topic) {
+                        case "cooking":
+                            photoSrc = cookingPhoto;
+                            photoAlt = "colourful cookery";
+                            break;
+                        case "coding":
+                            photoSrc = codingPhoto;
+                            photoAlt = "messy computer code";
+                            break;
+                        case "football":
+                        // if anything goes awry, the default will render a football photo
+                        default:
+                            photoSrc = footballPhoto;
+                            photoAlt = "football stadium";
+                            break;
+                    }
                     return (
                         <li key={article.article_id}>
                             <Link to={`/articles/${article.article_id}`}>
-                                <img
-                                    src="https://picsum.photos/600/350"
-                                    alt="randomly generated image"
-                                />
+                                <img src={photoSrc} alt={photoAlt} />
                                 <br />
                                 <strong>{article.title}</strong> <i>by</i>{" "}
                                 {article.author}
