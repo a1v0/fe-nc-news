7ee48dfbf95b518fdc4e4898d04bea3b12f5140d
avc285 <avc285@gmail.com>
2022-12-08 14:59:36 +0000

    allow for persistent logged in user w/ Web Storage

--- src/contexts/UserProvider.jsx
+++ src/contexts/UserProvider.jsx
@@ -3,13 +3,11 @@ import { createContext, useEffect, useState } from "react";
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-    // console.log(JSON.parse(window.sessionStorage.loggedInUser));
-    // const initialLoggedInState = Object.keys(window.sessionStorage.loggedInUser)
-    //     .length
-    //     ? JSON.parse(window.sessionStorage.loggedInUser)
-    //     : null;
-    // const [loggedInUser, setLoggedInUser] = useState(initialLoggedInState);
-    const [loggedInUser, setLoggedInUser] = useState(null);
+    const initialLoggedInState =
+        Object.keys(JSON.parse(window.sessionStorage.loggedInUser)).length === 0
+            ? null
+            : JSON.parse(window.sessionStorage.loggedInUser);
+    const [loggedInUser, setLoggedInUser] = useState(initialLoggedInState);
 
     useEffect(() => {
         if (loggedInUser === null) {
