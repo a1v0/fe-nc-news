4130a196893d958fad07842ab8584505363f4431
avc285 <avc285@gmail.com>
2022-12-08 12:10:34 +0000

    add error handling to Article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -6,25 +6,36 @@ import cookingPhoto from "../misc/maarten-van-den-heuvel-EzH46XCDQRY-unsplash.jp
 import codingPhoto from "../misc/markus-spiske-cvBBO4PzWPg-unsplash.jpg";
 import footballPhoto from "../misc/thomas-serer-r-xKieMqL34-unsplash.jpg";
 import placeholderPhoto from "../misc/elementor-placeholder-image.jpg";
+import NotFound from "./NotFound";
 
 export default function Article() {
     const { article_id } = useParams();
     const [article, setArticle] = useState({});
     const [relatedArticles, setRelatedArticles] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [isContentNotFound, setIsContentNotFound] = useState(false);
 
     useEffect(() => {
         setIsLoading(true);
         getArticle(article_id)
             .then((article) => {
-                setArticle(article);
-                return article;
+                if (article) {
+                    setIsContentNotFound(false);
+                    // articles only set if an articles object is passed. Any other response would be some sort of error
+                    setArticle(article);
+                    return article;
+                } else {
+                    return Promise.reject();
+                }
             })
             .then((article) => {
                 return getArticles(article.topic, 4);
             })
             .then((articles) => {
                 setRelatedArticles(articles);
+            })
+            .catch(() => {
+                setIsContentNotFound(true);
             });
     }, [article_id]);
 
@@ -56,52 +67,58 @@ export default function Article() {
         <main className="article-parent">
             <article className="Article">
                 {!isLoading ? (
-                    <>
-                        <h1>{article.title}</h1>
-                        <div className="author-and-topic">
-                            <p>
-                                <strong>&mdash; {article.author}</strong>{" "}
-                                (author)
-                            </p>
-                            <p>#{article.topic}</p>
-                        </div>
-                        <section>
-                            <img src={photoSrc} alt={photoAlt} />
-                            <p className="article-body">{article.body}</p>
-                        </section>
-                    </>
+                    isContentNotFound ? (
+                        <NotFound missingPiece="article" />
+                    ) : (
+                        <>
+                            <h1>{article.title}</h1>
+                            <div className="author-and-topic">
+                                <p>
+                                    <strong>&mdash; {article.author}</strong>{" "}
+                                    (author)
+                                </p>
+                                <p>#{article.topic}</p>
+                            </div>
+                            <section>
+                                <img src={photoSrc} alt={photoAlt} />
+                                <p className="article-body">{article.body}</p>
+                            </section>
+                        </>
+                    )
                 ) : (
                     <p>Loading article...</p>
                 )}
                 {!isLoading ? (
-                    <aside className="related-content">
-                        <h2>Related Content</h2>
-                        <ul>
-                            {relatedArticles.map((relatedArticle) => {
-                                if (
-                                    relatedArticle.article_id ===
-                                    Number(article_id)
-                                ) {
-                                    return null;
-                                }
+                    isContentNotFound ? null : (
+                        <aside className="related-content">
+                            <h2>Related Content</h2>
+                            <ul>
+                                {relatedArticles.map((relatedArticle) => {
+                                    if (
+                                        relatedArticle.article_id ===
+                                        Number(article_id)
+                                    ) {
+                                        return null;
+                                    }
 
-                                return (
-                                    <li key={relatedArticle.article_id}>
-                                        <Link
-                                            to={`/articles/${relatedArticle.article_id}`}
-                                        >
-                                            {relatedArticle.title}
-                                        </Link>
-                                    </li>
-                                );
-                            })}
-                        </ul>
-                    </aside>
+                                    return (
+                                        <li key={relatedArticle.article_id}>
+                                            <Link
+                                                to={`/articles/${relatedArticle.article_id}`}
+                                            >
+                                                {relatedArticle.title}
+                                            </Link>
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+                        </aside>
+                    )
                 ) : (
                     <p>Loading related content...</p>
                 )}
             </article>
-            <Comments article_id={article_id} />
+            {isContentNotFound ? null : <Comments article_id={article_id} />}
         </main>
     );
 }
